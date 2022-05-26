017b36d58cb67dc14027b0055850e60cb7719783
Christof Lauriers <christof.lauriers@gmail.com>
2022-05-26 14:14:44 +0200

    Fixed nullability issues

--- src/DomainDrivenBase.Domain/DomainEvent.cs
+++ src/DomainDrivenBase.Domain/DomainEvent.cs
@@ -2,10 +2,10 @@
 {
     public abstract class DomainEvent
     {
-        public string Id { get; protected init; }
-        public string IssuedFor { get; private set; }
+        public string Id { get; protected init; } = string.Empty;
+        public string IssuedFor { get; private set; } = string.Empty;
         public long OccuredOn { get; private set; }
-        public string CorrelationId { get; private set; }
+        public string CorrelationId { get; private set; } = string.Empty;
 
         protected DomainEvent()
         {
