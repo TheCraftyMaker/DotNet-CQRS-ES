2a52ebfa4aa97961472777e975d00d7a0460957d
Christof Lauriers <christof.lauriers@gmail.com>
2022-05-26 13:29:03 +0200

    Messaging

---
+++ src/DomainDriven.Infrastructure/Bus/InternalMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace DomainDriven.Infrastructure.Bus
+{
+    public sealed class InternalMessage
+    {
+        public bool Handled { get; private set; }
+        public string StreamName { get; }
+        public int Version { get; }
+        public string StreamType { get; }
+
+        public InternalMessage(string streamName, string streamType, int version)
+        {
+            StreamName = streamName;
+            StreamType = streamType;
+            Version = version;
+            Handled = false;
+        }
+
+        public void SetHandled()
+        {
+            Handled = true;
+        }
+    }
+}
