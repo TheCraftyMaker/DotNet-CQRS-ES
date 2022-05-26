7880d33939147135d7e79d811e5d55e2650a4243
Christof Lauriers <christof.lauriers@gmail.com>
2022-05-26 14:52:20 +0200

    Added sourced entity tests

---
+++ src/Tests/DomainDriven.Tests.Domain/SourcedEntitySpecification.cs
@@ -0,0 +1,101 @@
+﻿using DomainDriven.Tests.Application.SpecificationImplementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainDriven.Tests.Application
+{
+    [TestClass]
+    public class SourcedEntitySpecification : SourcedEntitySpecificationImplementation
+    {
+        [TestMethod]
+        public void Two_Entities_With_Same_Identifier_Should_Be_Equal()
+        {
+            GIVEN_An_Entity();
+            WHEN_Creating_An_Entity_With_The_Same_Identifier();
+            THEN_Both_Value_Objects_Should_Be_Equal();
+        }
+
+        [TestMethod]
+        public void Two_Entities_With_Same_Identifier_Should_Have_The_Same_Hashcode()
+        {
+            GIVEN_An_Entity();
+            WHEN_Creating_An_Entity_With_The_Same_Identifier();
+            THEN_Hashcode_Of_Both_Entities_Should_Be_The_Same();
+        }
+
+        [TestMethod]
+        public void Creating_An_Entity_With_Identifier_Should_Have_The_Correct_Identifier_Value()
+        {
+            WHEN_Creating_An_Entity_Object_With_Identifier();
+            THEN_The_Identifier_Should_Be_Set_With_The_Correct_Value();
+        }
+
+        [TestMethod]
+        public void Creating_An_Entity_With_Identifier_Should_Have_The_Correct_Current_Version()
+        {
+            WHEN_Creating_An_Entity_Object_With_Identifier();
+            THEN_The_Current_Version_Should_Be_Set_To_Zero();
+        }
+
+        [TestMethod]
+        public void Creating_An_Entity_With_Identifier_Should_Have_The_Correct_Next_Version()
+        {
+            WHEN_Creating_An_Entity_Object_With_Identifier();
+            THEN_The_Next_Version_Should_Be_Set_To_One();
+        }
+
+        [TestMethod]
+        public void Initializing_An_Entity_With_A_Stream_Should_Set_The_Correct_Current_Version()
+        {
+            WHEN_Creating_An_Entity_Object_From_Source_Stream();
+            THEN_The_Current_Version_Should_Be_Set_With_The_Stream_Version();
+        }
+
+        [TestMethod]
+        public void Initializing_An_Entity_With_A_Stream_Should_Set_The_Correct_Next_Version()
+        {
+            WHEN_Creating_An_Entity_Object_From_Source_Stream();
+            THEN_The_Next_Version_Should_Be_Set_With_Stream_Version_Plus_One();
+        }
+
+        [TestMethod]
+        public void Initializing_An_Entity_With_A_Stream_Should_Invoke_When_Method_For_Every_Event_In_The_Strea()
+        {
+            WHEN_Creating_An_Entity_Object_From_Source_Stream();
+            THEN_The_When_Method_Should_Be_Invoked_As_Many_Times_As_There_Are_Events_In_The_Stream();
+        }
+
+        [TestMethod]
+        public void Applying_An_Event_To_Entity_Should_Add_Event_To_Applied_Collection()
+        {
+            GIVEN_An_Entity();
+            WHEN_Applying_A_Domain_Event();
+            THEN_The_Event_Should_Be_Added_To_The_Applied_Collection();
+        }
+
+        [TestMethod]
+        public void Marking_An_Event_Committed_Should_Remove_The_Event_From_Applied_Collection()
+        {
+            GIVEN_An_Entity();
+            WHEN_Applying_A_Domain_Event();
+            WHEN_Marking_A_Domain_Event_As_Committed();
+            THEN_The_Current_Version_Should_Be_Set_With_Plus_One();
+            THEN_The_Event_Should_Be_Removed_From_The_Applied_Collection();
+        }
+
+        [TestMethod]
+        public void Applying_An_Event_To_Entity_Should_Invoke_When_Method()
+        {
+            GIVEN_An_Entity();
+            WHEN_Applying_A_Domain_Event();
+            THEN_The_When_Method_Should_Be_Invoked_Once();
+        }
+
+        [TestMethod]
+        public void Applying_An_Event_To_Entity_Should_Invoke_EnsureValidState_Method()
+        {
+            GIVEN_An_Entity();
+            WHEN_Applying_A_Domain_Event();
+            THEN_The_EnsureValidState_Method_Should_Be_Invoked_Once();
+        }
+    }
+}
---
+++ src/Tests/DomainDriven.Tests.Domain/SpecificationImplementation/SourcedEntitySpecificationImplementation.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DomainDriven.Domain;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainDriven.Tests.Application.SpecificationImplementation
+{
+    public abstract class SourcedEntitySpecificationImplementation
+    {
+        private Guid _idValue;
+        private int _streamVersion;
+        private List<TestEvent> _stream;
+
+        private TestEntity _entityObject;
+        private TestEntity _otherEntityObject;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _idValue = Guid.NewGuid();
+            _stream = new List<TestEvent>
+            {
+                new(Guid.NewGuid().ToString(), 2),
+                new(Guid.NewGuid().ToString(), 1)
+            };
+            _streamVersion = _stream.Count;
+        }
+
+        protected void GIVEN_An_Entity()
+        {
+            _streamVersion = 0;
+            _entityObject = new TestEntity(TestId.FromExisting(_idValue.ToString()));
+        }
+
+        protected void WHEN_Creating_An_Entity_Object_With_Identifier()
+        {
+            _streamVersion = 0;
+            _entityObject = new TestEntity(TestId.FromExisting(_idValue.ToString()));
+        }
+
+        protected void WHEN_Creating_An_Entity_Object_From_Source_Stream()
+        {
+            _entityObject = new TestEntity(_stream, _streamVersion, TestId.FromExisting(_idValue.ToString()));
+        }
+
+        protected void WHEN_Creating_An_Entity_With_The_Same_Identifier()
+        {
+            _otherEntityObject = new TestEntity(TestId.FromExisting(_idValue.ToString()));
+        }
+
+        protected void WHEN_Applying_A_Domain_Event()
+        {
+            _entityObject.ApplyGateway(_stream.First());
+        }
+
+        protected void WHEN_Marking_A_Domain_Event_As_Committed()
+        {
+            _entityObject.MarkCommittedGateway();
+        }
+
+        protected void THEN_Both_Value_Objects_Should_Be_Equal()
+        {
+            _entityObject.Should().NotBeNull();
+            _otherEntityObject.Should().NotBeNull();
+
+            _entityObject.Equals(_otherEntityObject).Should().BeTrue();
+        }
+
+        protected void THEN_Hashcode_Of_Both_Entities_Should_Be_The_Same()
+        {
+            _entityObject.Should().NotBeNull();
+            _otherEntityObject.Should().NotBeNull();
+
+            var hashCode = _entityObject.GetHashCode();
+            var otherHashCode = _otherEntityObject.GetHashCode();
+
+            hashCode.Should().Be(otherHashCode);
+        }
+
+        protected void THEN_The_Identifier_Should_Be_Set_With_The_Correct_Value()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.Id.Value.Should().Be(_idValue);
+        }
+
+        protected void THEN_The_Current_Version_Should_Be_Set_With_The_Stream_Version()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.CurrentVersion.Should().Be(_streamVersion);
+        }
+
+        protected void THEN_The_Current_Version_Should_Be_Set_With_Plus_One()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.CurrentVersion.Should().Be(_streamVersion + 1);
+        }
+
+        protected void THEN_The_Next_Version_Should_Be_Set_With_Stream_Version_Plus_One()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.NextVersion.Should().Be(_streamVersion + 1);
+        }
+
+        protected void THEN_The_Current_Version_Should_Be_Set_To_Zero()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.CurrentVersion.Should().Be(0);
+        }
+
+        protected void THEN_The_Next_Version_Should_Be_Set_To_One()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.NextVersion.Should().Be(1);
+        }
+
+        protected void THEN_The_When_Method_Should_Be_Invoked_As_Many_Times_As_There_Are_Events_In_The_Stream()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.WhenInvocationCount.Should().Be(_stream.Count);
+        }
+
+        protected void THEN_The_Event_Should_Be_Added_To_The_Applied_Collection()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.Applied.Should()
+                .HaveCount(1)
+                .And.ContainSingle(x => x.Equals(_stream.First()));
+        }
+
+        protected void THEN_The_Event_Should_Be_Removed_From_The_Applied_Collection()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.Applied.Should()
+                .HaveCount(0)
+                .And.NotContain(x => x.Equals(_stream.First()));
+        }
+
+        protected void THEN_The_When_Method_Should_Be_Invoked_Once()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.WhenInvocationCount.Should().Be(1);
+        }
+
+        protected void THEN_The_EnsureValidState_Method_Should_Be_Invoked_Once()
+        {
+            _entityObject.Should().NotBeNull();
+            _entityObject.EnsureValidStateCount.Should().Be(1);
+        }
+
+        public class TestEntity : SourcedEntity<TestId, DomainEvent>
+        {
+            public int WhenInvocationCount { get; set; }
+            public int EnsureValidStateCount { get; set; }
+
+            public TestEntity(TestId testId)
+                : base(testId)
+            {
+            }
+
+            public TestEntity(IEnumerable<DomainEvent> stream, int streamVersion, TestId testId)
+                : base(stream, streamVersion, testId)
+            {
+            }
+
+            public void ApplyGateway(DomainEvent @event)
+            {
+                Apply(@event);
+            }
+
+            public void MarkCommittedGateway()
+            {
+                MarkCommitted();
+            }
+
+            public void When(TestEvent @event)
+            {
+                WhenInvocationCount += 1;
+            }
+
+            protected override void EnsureValidState()
+            {
+                EnsureValidStateCount += 1;
+            }
+        }
+
+        public class TestEvent : DomainEvent
+        {
+            public int Version { get; set; }
+
+            public TestEvent(string id, int version)
+            {
+                Id = id;
+                Version = version;
+            }
+        }
+        
+        public class TestId : Value<TestId>
+        {
+            public Guid Value { get; }
+
+            private TestId()
+            {
+                Value = Guid.NewGuid();
+            }
+
+            private TestId(string referenceId)
+            {
+                if (!Guid.TryParse(referenceId, out var id))
+                {
+                    throw new ArgumentException("Invalid testId", 
+                        nameof(referenceId));
+                }
+                Value = id;
+            }
+
+            public static TestId FromExisting(string referenceId)
+            {
+                return new(referenceId);
+            }
+            
+            public static implicit operator TestId(string id)
+            {
+                return new(id);
+            }
+
+            public static implicit operator string(TestId testId)
+            {
+                return testId.Value.ToString();
+            }
+        }
+    }
+}
